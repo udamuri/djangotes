85a13843775f4f0d229d9ebf01d2dc25b3d849b3
udamuri <udamuri@gmail.com>
2019-04-23 13:58:41 +0700

    change database mysql

--- sinergicloud/posts/admin.py
+++ sinergicloud/posts/admin.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from .models import Post, Category
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'slug')
-    #list_filter = ['pub_date']
+    list_display = ('title', 'category', 'user', 'slug')
+    list_filter = ('category', 'user')
     search_fields = ['title']
 
 admin.site.register(Post, PostAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     #list_display = ('name', 'slug', 'parent', 'pub_date', 'was_published_recently')
-    #list_filter = ['pub_date']
+    list_filter = ()
     search_fields = ['name']
 
 admin.site.register(Category, CategoryAdmin)
--- sinergicloud/sinergicloud/settings.py
+++ sinergicloud/sinergicloud/settings.py
@@ -80,13 +80,21 @@ WSGI_APPLICATION = 'sinergicloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+##DATABASES = {
+##    'default': {
+##        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##    }
+##}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
